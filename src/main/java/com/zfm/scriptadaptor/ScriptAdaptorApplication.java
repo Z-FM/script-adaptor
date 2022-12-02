17da75f6128ae5d84c939ed5d316d0458b8d96b1
张飞明60224 <feiming.zhang@going-link.com>
2022-12-02 17:38:57 +0800

    init project

---
+++ src/main/java/com/zfm/scriptadaptor/ScriptAdaptorApplication.java
@@ -0,0 +1,13 @@
+package com.zfm.scriptadaptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptAdaptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptAdaptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zfm/scriptadaptor/ScriptAdaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfm.scriptadaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptAdaptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
